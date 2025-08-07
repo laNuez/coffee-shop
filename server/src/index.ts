726ba571f3c61b58a514c007c708cc0ffeff6749
laNuez <77354823+laNuez@users.noreply.github.com>
2025-08-06 19:20:43 -0700

    feat(server): add user context

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,24 @@
-import { Hono } from "hono";
-import { cors } from "hono/cors";
-import type { ApiResponse, LoginForm, Token } from "shared/dist";
-import { drizzle } from "drizzle-orm/libsql";
-import { mockProducts } from "./mock";
+import { Hono, type Context, type Next } from 'hono'
+import { cors } from 'hono/cors'
+import type { ApiResponse, LoginForm, Token } from 'shared/dist'
+import { drizzle } from 'drizzle-orm/libsql'
+import { mockProducts } from './mock'
 import { userInsertSchema, usersTable } from './db/schema'
 import bcrypt from 'bcrypt'
-import { ConsoleLogWriter, DrizzleQueryError, eq, or } from 'drizzle-orm'
+import { DrizzleQueryError, eq, or } from 'drizzle-orm'
 import { createUser } from './db/mutations'
 import z from 'zod'
-import { getUserByUsername } from "./db/queries";
-import jwt from 'jsonwebtoken'
+import { getUserByUsername } from './db/queries'
+import { contextStorage, getContext } from 'hono/context-storage'
+import { verify, sign } from 'hono/jwt'
 
-const {JWT_SECRET} = process.env
+const { JWT_SECRET } = process.env
+
+type Variables = {
+  Variables: {
+    currentUser: null | { username: string }
+  }
+}
 
 export const db = drizzle({
   connection: {
@@ -20,9 +27,40 @@ export const db = drizzle({
   },
 })
 
-export const app = new Hono()
+const userContext = async (c: Context, next: Next) => {
+  const auth = c.req.header('Authorization')
+  if (!auth) {
+    c.set('currentUser', null)
+    return next()
+  }
+  const token = auth.split('Bearer ')[1]
+  if (!token) {
+    c.set('currentUser', null)
+    return next()
+  }
+
+  try {
+    const user = await verify(token, JWT_SECRET!)
+    c.set('currentUser', user)
+    return next()
+  } catch (error) {
+    console.log(error)
+    c.set('currentUser', null)
+    next()
+  }
+}
+
+export const app = new Hono<Variables>()
 
   .use(cors())
+
+  .use(contextStorage())
+  .use('/auth/*', userContext)
+
+  .get('/auth/page', (c) => {
+    console.log(getContext<Variables>().var.currentUser)
+    return c.text('You are authorized')
+  })
 
   .get('/', (c) => {
     return c.text('Hello Hono!')
@@ -54,10 +92,10 @@ export const app = new Hono()
       .where(
         or(
           eq(usersTable.username, data.username),
-          eq(usersTable.email, data.email),
+          eq(usersTable.email, data.email)
         )
       )
-    
+
     if (username) {
       c.status(400)
       return c.json({ error: 'username or email already exists' })
@@ -88,34 +126,36 @@ export const app = new Hono()
     const body = await c.req.json()
     const parseLoginForm = z.object({
       username: z.string(),
-      password: z.string()
+      password: z.string(),
     })
 
     const { success, data, error } = parseLoginForm.safeParse(body)
     if (!success) {
       c.status(400)
-      return c.json({error: 'invalid request'})
+      return c.json({ error: 'invalid request' })
     }
 
     const [user] = await getUserByUsername(data.username)
     if (!user) {
       c.status(400)
-      return c.json({error: 'wrong username or password'})
+      return c.json({ error: 'wrong username or password' })
     }
 
     const correct = await bcrypt.compare(data.password, user.password)
     if (!correct) {
       c.status(400)
-      return c.json({error: 'wrong username or password'})
+      return c.json({ error: 'wrong username or password' })
     }
-    
-    const {password, ...remaining} = user
 
-    const accessToken = jwt.sign( {...remaining}, JWT_SECRET!, { expiresIn: '1h'})
+    const { password, ...remaining } = user
+
+    // expires in 60 minutes
+    const exp = Math.floor(Date.now() / 1000) + 60 * 60
+    const accessToken = await sign({ ...remaining, exp }, JWT_SECRET!)
     const resData: Token = {
-      accessToken
+      accessToken,
     }
     return c.json(resData)
   })
 
-export default app;
+export default app
